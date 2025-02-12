2d21dcd477c746f1f58e2c7a8051b39f15f62251
Thales Bergamin <thalesbergamim@gmail.com>
2025-02-11 21:49:53 -0300

    alterando settings

--- app/settings.py
+++ app/settings.py
@@ -89,12 +89,12 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
         'HOST': config('DB_HOST', default='localhost'),
-        'PORT': config('DB_PORT', default='5432'),
+        'PORT': config('DB_PORT', default='3306'),
     }
 }
 
